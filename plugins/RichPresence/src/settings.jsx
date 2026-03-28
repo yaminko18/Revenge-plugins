c9e7c7ad0819157d4af9526bda7a4a7e3a35223a
yaminko18 <yaminko18@proton.me>
2026-03-28 07:11:04 +0100

    Update settings.jsx

--- plugins/RichPresence/src/settings.jsx
+++ plugins/RichPresence/src/settings.jsx
@@ -1,25 +1,24 @@
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
 import { Forms } from "@vendetta/ui/components";
-import { ReactNative } from "@vendetta/metro/common";
+import { ReactNative, React } from "@vendetta/metro/common";
 
-const { FormSwitchRow } = Forms;
+const { FormSwitchRow, FormSection } = Forms;
 const { ScrollView } = ReactNative;
 
-export default () => {
-    // useProxy zabezpečí, že sa UI okamžite aktualizuje po kliknutí
+export default function Settings() {
     useProxy(storage);
 
     return (
         <ScrollView style={{ flex: 1 }}>
-            <FormSwitchRow
-                label="Priamo prepísať text správy"
-                subLabel="ZAPNUTÉ: Text sa prepíše priamo v bubline. VYPNUTÉ: Pôvodný text ostane a preklad sa zobrazí pod ním ako červená systémová správa."
-                value={storage.overwriteMode ?? false}
-                onValueChange={(value) => {
-                    storage.overwriteMode = value;
-                }}
-            />
+            <FormSection title="Nastavenia prekladu" titleStyle={{ marginTop: 20 }}>
+                <FormSwitchRow
+                    label="Priamo prepísať text"
+                    subLabel="ZAPNUTÉ: Prepíše text v bubline. VYPNUTÉ: Zobrazí preklad pod správou (červený text)."
+                    value={storage.overwriteMode ?? false}
+                    onValueChange={(v) => storage.overwriteMode = v}
+                />
+            </FormSection>
         </ScrollView>
     );
-};
+}
