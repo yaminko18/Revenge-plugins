aba9dec733d05f32519ae7320c9a2d8f67caf0bc
yaminko18 <yaminko18@proton.me>
2026-03-28 07:18:49 +0100

    Update index.js

--- plugins/RichPresence/src/index.js
+++ plugins/RichPresence/src/index.js
@@ -27,31 +27,32 @@ let unpatch;
 function processMessage(msg, delay = 150) {
     if (!msg?.content || !monthRegex.test(msg.content)) return;
 
-    const translated = msg.content.replace(monthRegex, (match) => {
+    const translatedText = msg.content.replace(monthRegex, (match) => {
         const norm = match.replace(/\s+/g, ' ').toLowerCase();
         let res = czechToSlovakMonths[norm];
         if (!res) return match;
-        return match[0] === match[0].toUpperCase() ? res.charAt(0).toUpperCase() + res.slice(1) : res;
+
+        // Zachovanie veľkého písmena
+        if (match[0] === match[0].toUpperCase()) {
+            res = res.charAt(0).toUpperCase() + res.slice(1);
+        }
+
+        // Ak je zapnutý režim "Obidve", vrátime: pôvodné (**preklad**)
+        if (storage.overwriteMode && storage.showBoth) {
+            return `${match} (**${res}**)`;
+        }
+        
+        return res;
     });
 
     if (storage.overwriteMode) {
-        msg.content = translated;
+        msg.content = translatedText;
     } else {
-        // Použijeme delay, aby sme nezahltili Dispatcher pri načítaní histórie
         setTimeout(() => {
             FluxDispatcher.dispatch({
                 type: "MESSAGE_EDIT_FAILED_AUTOMOD",
-                messageData: { 
-                    type: 1, 
-                    message: { 
-                        channelId: msg.channel_id || msg.channelId, 
-                        messageId: msg.id 
-                    } 
-                },
-                errorResponseBody: { 
-                    code: 200000, 
-                    message: `🇸🇰 Preklad: ${translated}` 
-                },
+                messageData: { type: 1, message: { channelId: msg.channel_id || msg.channelId, messageId: msg.id } },
+                errorResponseBody: { code: 200000, message: `🇸🇰 Preklad: ${translatedText}` },
             });
         }, delay);
     }
@@ -60,29 +61,16 @@ function processMessage(msg, delay = 150) {
 export default {
     settings,
     onLoad() {
-        if (storage.overwriteMode === undefined) storage.overwriteMode = false;
+        storage.overwriteMode = storage.overwriteMode ?? false;
+        storage.showBoth = storage.showBoth ?? false;
 
         unpatch = patchBefore("dispatch", FluxDispatcher, ([event]) => {
-            // Nová správa
-            if (event?.type === "MESSAGE_CREATE" && event.message) {
-                processMessage(event.message, 150);
-            }
-            
-            // História správ - pridávame postupné oneskorenie
+            if (event?.type === "MESSAGE_CREATE" && event.message) processMessage(event.message, 150);
             if (event?.type === "LOAD_MESSAGES_SUCCESS" && event.messages) {
-                event.messages.forEach((msg, index) => {
-                    // Každá správa v histórii dostane o 50ms väčší odstup
-                    processMessage(msg, 200 + (index * 50));
-                });
+                event.messages.forEach((msg, i) => processMessage(msg, 200 + (i * 50)));
             }
-
-            // Editácia správy
-            if (event?.type === "MESSAGE_UPDATE" && event.message) {
-                processMessage(event.message, 150);
-            }
+            if (event?.type === "MESSAGE_UPDATE" && event.message) processMessage(event.message, 150);
         });
     },
-    onUnload() {
-        if (unpatch) unpatch();
-    }
+    onUnload() { if (unpatch) unpatch(); }
 };
